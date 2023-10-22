cff7fe210d4b9c7f8a42d7119fbe369839ccea86
zeroxiru <zerox.iru@gmail.com>
2023-10-22 09:02:16 +0600

    woriking with multiple files.

--- Sprint106.1/FlaskAppLearning/flask_app_with_jinja_2/app.py
+++ Sprint106.1/FlaskAppLearning/flask_app_with_jinja_2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -15,6 +15,31 @@ def index():
 @app.route('/all-users')
 def all_ausers():
     return render_template('all_users_with_tab.html', users=users)
+
+@app.route('/update_country', methods=['GET', 'POST'])
+def update_country():
+    if request.method == 'POST':
+
+        #Get the username and the  new country from the form
+        name = request.form.get('name')
+        new_country = request.form.get('country')
+
+        # update the  user's country in the dictionary
+        if name in users:
+            users[name]['country'] = new_country
+
+        return redirect(url_for('all-users'))
+        # Get the updated country from the form
+        update_country = request.form['country']
+
+        # # Update the 'country' attribute while keeping the rest unchanged
+        # users['country'] = update_country
+        #
+        # #Return a response, render a template, or redirect as needed
+        # return f'Updated country to: {update_country}'
+
+        #render_template('.html', users)
+    return render_template('update_users.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
